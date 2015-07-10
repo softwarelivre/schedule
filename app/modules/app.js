512b52a0738a5d6e19c498dd87eb5652c3855c16
Felipe Mobus <fmobus@gmail.com>
2015-07-10 11:35:27 -0300

    added recordings links

--- app/modules/app.js
+++ app/modules/app.js
@@ -24,7 +24,7 @@
             main:   { controller: 'GridController',   templateUrl: 'modules/templates/grid.html' }
           },
           resolve: {
-            currentDay: function($stateParams) { return $stateParams.day || '2015-07-08'; }
+            currentDay: function($stateParams) { return $stateParams.day || '2015-07-10'; }
           }
         })
         .state('talk', {
